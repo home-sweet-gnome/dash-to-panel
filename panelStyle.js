c65bb1758f448c6a41f100d011c02af386ad15da
jderose9 <coder@derose.io>
2017-01-03 20:29:49 -0500

    apply multiple styles in one recursion pass

--- panelStyle.js
+++ panelStyle.js
@@ -56,7 +56,7 @@ const taskbarPanelStyle = new Lang.Class({
             "leftbox-padding",
             "tray-padding",
             "status-icon-padding"
-        ].forEach(function(configName) {
+        ].forEach(function(configName) { // fix foreach
             settings.connect('changed::' + configName, Lang.bind(this, function () {
                 this._removeStyles();
                 this._applyStyles();
@@ -65,69 +65,94 @@ const taskbarPanelStyle = new Lang.Class({
     },
 
     _applyStyles: function() {
+        this._rightBoxOperations = [];
+        
         let trayPadding = -1; //this._dtpSettings.get_int('tray-padding');
-        let setTrayPadding = null;
         if(trayPadding >= 0) {
             let trayPaddingStyleLine = '-natural-hpadding: %dpx'.format(trayPadding);
             if (trayPadding < 6) {
                 trayPaddingStyleLine += '; -minimum-hpadding: %dpx'.format(trayPadding);
             }
-            let trayPaddingChildren = this.panel._rightBox.get_children()
-                .concat(this.panel._centerBox.get_children());
-            setTrayPadding = Lang.bind(this, function (actor) {
-                this._overrideStyle(actor, trayPaddingStyleLine, function (actor) {
-                    return (actor.has_style_class_name && actor.has_style_class_name('panel-button'));
-                }, 2);
+            let operation = {};
+            operation.compareFn = function (actor) {
+                return (actor.has_style_class_name && actor.has_style_class_name('panel-button'));
+            };
+            operation.applyFn = Lang.bind(this, function (actor) {
+                global.log('applying ' + trayPaddingStyleLine + ' to ' + actor.get_style_class_name());
+                this._overrideStyle(actor, trayPaddingStyleLine);
             });
-            for (let i = 0; i < trayPaddingChildren.length; ++i)
-                setTrayPadding(trayPaddingChildren[i]);
+            this._rightBoxOperations.push(operation);
         }
 
-        let leftboxPadding = -1; //this._dtpSettings.get_int('leftbox-padding');
-        let setLeftboxPadding = null;
-        if(leftboxPadding >= 0) {
-            let leftboxPaddingStyleLine = '-natural-hpadding: %dpx'.format(leftboxPadding);
-            let leftboxPaddingChildren = this.panel._leftBox.get_children()
-            setLeftboxPadding = Lang.bind(this, function (actor) {
-                this._overrideStyle(actor, leftboxPaddingStyleLine, function (actor) {
-                    return (actor.has_style_class_name && actor.has_style_class_name('panel-button'));
-                }, 2);
-            });
-            for (let i = 0; i < leftboxPaddingChildren.length; ++i)
-                setLeftboxPadding(leftboxPaddingChildren[i]);
-        }
-        
         let statusIconPadding = -1; //this._dtpSettings.get_int('status-icon-padding');
-        let setStatusIconPadding = null;
         if(statusIconPadding >= 0) {
             let statusIconPaddingStyleLine = 'padding-left: %dpx; padding-right: %dpx'.format(statusIconPadding, statusIconPadding)
-            let statusIconPaddingChildren = this.panel.statusArea.aggregateMenu._indicators.get_children();
-            setStatusIconPadding = Lang.bind(this, function (actor) {
-                this._overrideStyle(actor, statusIconPaddingStyleLine, function (actor) {
-                        return (actor.has_style_class_name && actor.has_style_class_name('system-status-icon'));
-                    }, 2);
+            let operation = {};
+            operation.compareFn = function (actor) {
+                return (actor.has_style_class_name && actor.has_style_class_name('system-status-icon'));
+            };
+            operation.applyFn = Lang.bind(this, function (actor) {
+                this._overrideStyle(actor, statusIconPaddingStyleLine);
             });
-            for (let i = 0; i < statusIconPaddingChildren.length; ++i)
-                setStatusIconPadding(statusIconPaddingChildren[i]);
+            this._rightBoxOperations.push(operation);
         }
        
+        // center box has been moved next to the right box and will be treated the same
+        this._centerBoxOperations = this._rightBoxOperations;
+
+        this._leftBoxOperations = [];
+
+        let leftboxPadding = -1; //this._dtpSettings.get_int('leftbox-padding');
+        if(leftboxPadding >= 0) {
+            let leftboxPaddingStyleLine = '-natural-hpadding: %dpx'.format(leftboxPadding);
+            if (leftboxPadding < 6) {
+                leftboxPaddingStyleLine += '; -minimum-hpadding: %dpx'.format(leftboxPadding);
+            }
+            let operation = {};
+            operation.compareFn = function (actor) {
+                return (actor.has_style_class_name && actor.has_style_class_name('panel-button'));
+            };
+            operation.applyFn = Lang.bind(this, function (actor) {
+                this._overrideStyle(actor, leftboxPaddingStyleLine);
+            });
+            this._leftBoxOperations.push(operation);
+        }
+
+        if(this._rightBoxOperations.length) {
+            let children = this.panel._rightBox.get_children();
+            for(let i in children)
+                this._recursiveApply(children[i], this._rightBoxOperations);
+        }
+
+        if(this._centerBoxOperations.length) {
+            let children = this.panel._centerBox.get_children();
+            for(let i in children)
+                this._recursiveApply(children[i], this._centerBoxOperations);
+        }
+
+        if(this._leftBoxOperations.length) {
+            let children = this.panel._leftBox.get_children();
+            for(let i in children)
+                this._recursiveApply(children[i], this._leftBoxOperations);
+        }
+        
         /* connect signal */
         this._rightBoxActorAddedID = this.panel._rightBox.connect('actor-added',
             Lang.bind(this, function (container, actor) {
-                if(setTrayPadding)
-                    setTrayPadding(actor);
+                if(this._rightBoxOperations.length)
+                    this._recursiveApply(child, this._rightBoxOperations);
             })
         );
         this._centerBoxActorAddedID = this.panel._centerBox.connect('actor-added',
             Lang.bind(this, function (container, actor) {
-                if(setTrayPadding)
-                    setTrayPadding(actor);
+                if(this._centerBoxOperations.length)
+                    this._recursiveApply(child, this._centerBoxOperations);
             })
         );
         this._leftBoxActorAddedID = this.panel._leftBox.connect('actor-added',
             Lang.bind(this, function (container, actor) {
-                if(setLeftboxPadding)
-                    setLeftboxPadding(actor);
+                if(this._leftBoxOperations.length)
+                    this._recursiveApply(child, this._leftBoxOperations);
             })
         );
     },
@@ -141,37 +166,44 @@ const taskbarPanelStyle = new Lang.Class({
         if (this._leftBoxActorAddedID) 
             this.panel._leftBox.disconnect(this._leftBoxActorAddedID);
 
-        let children = this.panel._rightBox.get_children()
-            .concat(this.panel._centerBox.get_children());
-
-        if(children)
-            for (let i = 0; i < children.length; ++i)
-                this._restoreOriginalStyle(children[i], function (actor) {
-                    return (actor.has_style_class_name && actor.has_style_class_name('panel-button'));
-                }, 2);
-
-        children = this.panel.statusArea.aggregateMenu._indicators.get_children();
-
-        if(children)
-            for (let i = 0; i < children.length; ++i)
-                this._restoreOriginalStyle(children[i], function (actor) {
-                    return (actor.has_style_class_name && actor.has_style_class_name('system-status-icon'));
-                }, 2);
-    },
-
-    _overrideStyle: function(actor, styleLine, compareFn, maxSearchDepth, recurseCount) {
-        if(!recurseCount)
-            recurseCount = 1;
-
-        if(!compareFn(actor)) {
-            if(actor.get_children) {
-                let children = actor.get_children();
-                for(let idx in children)
-                    this._overrideStyle(children[idx], styleLine, compareFn, maxSearchDepth, recurseCount+1);
-            }
-            return;
+        if(this._rightBoxOperations.length) {
+            let children = this.panel._rightBox.get_children();
+            for(let i in children)
+                this._recursiveApply(children[i], this._rightBoxOperations, true);
         }
 
+        if(this._centerBoxOperations.length) {
+            let children = this.panel._centerBox.get_children();
+            for(let i in children)
+                this._recursiveApply(children[i], this._centerBoxOperations, true);
+        }
+
+        if(this._leftBoxOperations.length) {
+            let children = this.panel._leftBox.get_children();
+            for(let i in children)
+                this._recursiveApply(children[i], this._leftBoxOperations, true);
+        }
+    },
+
+    _recursiveApply: function(actor, operations, restore) {
+        for(let i in operations) {
+            let o = operations[i];
+            if(o.compareFn(actor))
+                if(restore)
+                    o.restoreFn ? o.restoreFn(actor) : this._restoreOriginalStyle(actor);
+                else
+                    o.applyFn(actor);
+        }
+
+        if(actor.get_children) {
+            let children = actor.get_children();
+            for(let i in children) {
+                this._recursiveApply(children[i], operations, restore);
+            }
+        }
+    },
+    
+    _overrideStyle: function(actor, styleLine) {
         if (actor._original_inline_style_ === undefined) {
             actor._original_inline_style_ = actor.get_style();
         }
@@ -194,26 +226,13 @@ const taskbarPanelStyle = new Lang.Class({
                         // TODO: a ._style_pending which prevents it rather than disconnect/connect?
                         actor.disconnect(actor._dtpPanelStyleSignalID);
                         delete actor._dtpPanelStyleSignalID;
-                        this._overrideStyle(actor, styleLine, compareFn, maxSearchDepth);
+                        this._overrideStyle(actor, styleLine);
                     }
                 }));
         }
     },
 
-    // see the note in overrideStyle about us having to recurse down to the first
-    // child of `actor` in order to find the container with style class name
-    // 'panel-button' (applying our style to the parent container won't work).
-    _restoreOriginalStyle: function(actor, compareFn, maxSearchDepth, recurseCount) {
-         if(!compareFn(actor)) {
-            
-            let children = actor.get_children();
-
-            if(children)
-                for(let idx in children)
-                    this._restoreOriginalStyle(children[idx], compareFn, maxSearchDepth, recurseCount+1);
-            return;
-         }
-
+    _restoreOriginalStyle: function(actor) {
         if (actor._dtpPanelStyleSignalID) {
             actor.disconnect(actor._dtpPanelStyleSignalID);
             delete actor._dtpPanelStyleSignalID;
