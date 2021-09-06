eec5ffe8bc06e2d20faafb77c88c2f369256883c
Philipp Unger <philipp.unger.1988@gmail.com>
2021-09-06 20:42:42 +0200

    use startInOverview flag

--- extension.js
+++ extension.js
@@ -92,6 +92,8 @@ function _enable() {
     Me.settings = Convenience.getSettings('org.gnome.shell.extensions.dash-to-panel');
     Me.desktopSettings = Convenience.getSettings('org.gnome.desktop.interface');
 
+    Main.layoutManager.startInOverview = !Me.settings.get_boolean('hide-overview-on-startup');
+
     if (Me.settings.get_boolean('hide-overview-on-startup') && Main.layoutManager._startingUp) {
         Main.sessionMode.hasOverview = false;
         Main.layoutManager.connect('startup-complete', () => {
