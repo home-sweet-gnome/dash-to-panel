85501a943c0026723a403135a3f26c05c1d460c3
Philipp Unger <philipp.unger.1988@gmail.com>
2022-04-06 20:44:28 +0200

    migrate box show desktop  dialog

--- prefs.js
+++ prefs.js
@@ -169,6 +169,7 @@ const Preferences = class {
             // dialogs
             this._builder.add_from_file(Me.path + '/ui/AnimateAppIconHoverOptions.ui');
             this._builder.add_from_file(Me.path + '/ui/BoxDotOptions.ui');
+            this._builder.add_from_file(Me.path + '/ui/BoxShowDesktopOptions.ui');
 
             this._builder.add_from_file(Me.path + '/ui/SettingsPosition.ui');
             let pagePosition = this._builder.get_object('position');
