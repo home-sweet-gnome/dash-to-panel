3c12a114eb9878ecc2d5ab2efdb8839cfb41c9fe
Philipp Unger <philipp.unger.1988@gmail.com>
2022-04-06 21:47:10 +0200

    migrate scroll panel options box dialog

--- prefs.js
+++ prefs.js
@@ -178,6 +178,7 @@ const Preferences = class {
             this._builder.add_from_file(Me.path + '/ui/BoxMiddleClickOptions.ui');
             this._builder.add_from_file(Me.path + '/ui/BoxOverlayShortcut.ui');
             this._builder.add_from_file(Me.path + '/ui/BoxSecondaryMenuOptions.ui');
+            this._builder.add_from_file(Me.path + '/ui/ScrollPanelOptionsBox.ui');
 
             // pages
             this._builder.add_from_file(Me.path + '/ui/SettingsPosition.ui');
