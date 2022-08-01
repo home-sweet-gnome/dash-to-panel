269aa77f0ef409d63a5ee21453298a07b3fb00fb
T99Rots <T99.Rots@gmail.com>
2022-08-01 11:59:26 +0200

    fixed the show desktop button not responding to touch events mentioned in #1676 by @catscratchedme

--- panel.js
+++ panel.js
@@ -1170,6 +1170,11 @@ var Panel = GObject.registerClass({
 
             this._setShowDesktopButtonStyle();
 
+            this._showDesktopButton.connect('touch-event', (actor, event) => {
+              if (event.type() == Clutter.EventType.TOUCH_BEGIN) {
+                this._onShowDesktopButtonPress();
+              }
+            });
             this._showDesktopButton.connect('button-press-event', () => this._onShowDesktopButtonPress());
             this._showDesktopButton.connect('enter-event', () => {
                 this._showDesktopButton.add_style_class_name(this._getBackgroundBrightness() ?
